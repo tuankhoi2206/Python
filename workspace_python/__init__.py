c78ce460780a6bc5ab4d7d7c0797572cc6af513f
KhoiVu <tuankhoi2206@gmail.com>
2018-09-15 21:38:56 +0700

    Description: Modify views Push by: Khoi Vu

--- workspace_python/__init__.py
+++ workspace_python/__init__.py
@@ -2,9 +2,12 @@ from pyramid.config import Configurator
 from pyramid.session import SignedCookieSessionFactory
 my_session_factory = SignedCookieSessionFactory('itsaseekreet')
 
+#
+# The global_config variable is these value config within the production.ini file.
+#
+
 def main(global_config, **settings):
-    """ This function returns a Pyramid WSGI application.
-    """
+    # This function returns a Pyramid WSGI application.
     config = Configurator(settings=settings)
     # Config templating with Jinja2
     config.include('pyramid_jinja2')
--- workspace_python/views.py
+++ workspace_python/views.py
@@ -2,36 +2,55 @@ from pyramid.view import view_config
 from pyramid.compat import escape
 from pyramid.response import Response
 import logging
+
 log = logging.getLogger(__name__)
+from pyramid.view import (
+    view_config,
+    view_defaults
+)
+
 
 # Routing
 
-@view_config(route_name='home', renderer='templates/mytemplate.jinja2')
-def my_view(request):
-    log.debug('Some home')
-    session = request.session
-    if 'counter' in session:
-        session['counter'] += 1
-    else:
-        session['counter'] = 0
-    return {'project': 'Workspace_Python'}
+@view_defaults(renderer='templates/home.jinja2')
+class TutorialViews:
+    def __init__(self, request):
+        self.request = request
 
+    @view_config(route_name='home')
+    def home(self):
+        return {'name': 'Home View'}
 
-# /howdy?name=alice which links to the next view
-@view_config(route_name='hello', renderer='templates/test.jinja2')
-def hello_view(request):
-    log.debug('Some message')
-    return dict()
+    @view_config(route_name='hello')
+    def hello(self):
+        return {'name': 'Hello View'}
 
-
-@view_config(route_name='hello_json', renderer='json')
-def hello_json(request):
-    return {
-        'project': 'Workspace_Python',
-        'project1': 'Workspace_Python1',
-        'project2': 'Workspace_Python2'
-    }
-
-@view_config(route_name='exception')
-def exception_view():
-    raise Exception()
+# @view_config(route_name='home', renderer='templates/home.jinja2')
+# def my_view(request):
+#     log.debug('Some home')
+#     session = request.session
+#     if 'counter' in session:
+#         session['counter'] += 1
+#     else:
+#         session['counter'] = 0
+#     return {'name': 'Hello View'}
+#
+#
+# # /howdy?name=alice which links to the next view
+# @view_config(route_name='hello', renderer='templates/test.jinja2')
+# def hello_view(request):
+#     log.debug('Some message')
+#     return dict()
+#
+#
+# @view_config(route_name='hello_json', renderer='json')
+# def hello_json(request):
+#     return {
+#         'project': 'Workspace_Python',
+#         'project1': 'Workspace_Python1',
+#         'project2': 'Workspace_Python2'
+#     }
+#
+# @view_config(route_name='exception')
+# def exception_view():
+#     raise Exception()
